2ab33b644304e92f302be212e8fa6fb0d3f99eb2
Lastfiver <ben.dascombe@route.agency>
2025-06-27 10:15:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div>
+      <h1>🎉 SUCCESS!</h1>
+      <p>Amplify Manager is working!</p>
+    </div>
+  );
+}
+
+export default App;
